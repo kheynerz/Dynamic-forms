0f192b1c6962af0c6ff0694d6fd8eff6739c1346
kheynerz <sebastiancr44@gmail.com>
2022-06-13 16:02:08 -0600

    Add formly to project and example form in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormlyFieldConfig} from '@ngx-formly/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'Dinamyc-forms';
+  title = 'Dinamyc-Forms'
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      fieldGroupClassName: 'display-flex',
+      fieldGroup: [
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'firstName',
+          templateOptions: {
+            label: 'First Name'
+          },
+        },
+        {
+          className: 'flex-2',
+          type: 'input',
+          key: 'lastName',
+          templateOptions: {
+            label: 'Last Name',
+          },
+          expressionProperties: {
+            'templateOptions.disabled': '!model.firstName',
+          },
+        },
+      ],
+    },
+    {
+      template: '<hr /><div><strong>Address:</strong></div>',
+    },
+    {
+      fieldGroupClassName: 'display-flex',
+      fieldGroup: [
+        {
+          className: 'flex-2',
+          type: 'input',
+          key: 'street',
+          templateOptions: {
+            label: 'Street',
+          },
+        },
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'cityName',
+          templateOptions: {
+            label: 'City',
+          },
+        },
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'zip',
+          templateOptions: {
+            type: 'number',
+            label: 'Zip',
+            max: 99999,
+            min: 0,
+            pattern: '\\d{5}',
+          },
+        },
+      ],
+    },
+    {
+      template: '<hr />',
+    },
+    {
+      type: 'input',
+      key: 'otherInput',
+      templateOptions: {
+        label: 'Other Input',
+      },
+    },
+    {
+      type: 'checkbox',
+      key: 'otherToo',
+      templateOptions: {
+        label: 'Other Checkbox',
+      },
+    },
+  ];
+
+  onSubmit() {
+    if (this.form.valid) {
+      alert(JSON.stringify(this.model, null, 2));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
+    FormlyMaterialModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    useValue: appearance
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
