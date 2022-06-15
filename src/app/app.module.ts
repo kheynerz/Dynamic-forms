e6212069b69515472308f18540d3874690f302ee
kheynerz <sebastiancr44@gmail.com>
2022-06-14 19:08:06 -0600

    Import of Formly material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 
+
+/*Form components*/
+
+//Slider
+import { FormlyMatSliderModule } from '@ngx-formly/material/slider';
+
+//Toggle
+import { FormlyMatToggleModule } from '@ngx-formly/material/toggle';
+
+//DatePicker
+import { FormlyMatDatepickerModule } from '@ngx-formly/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 const appearance: MatFormFieldDefaultOptions = {
   appearance: 'outline'
 };
@@ -23,12 +37,18 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatButtonModule,
     ReactiveFormsModule,
+
     FormlyModule.forRoot({
       validationMessages: [
         { name: 'required', message: 'This field is required' },
       ],
-    }),
-    FormlyMaterialModule
+    }), 
+    FormlyMaterialModule,
+      //Form Components
+    FormlyMatSliderModule,
+    FormlyMatToggleModule,
+    FormlyMatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [
     {
